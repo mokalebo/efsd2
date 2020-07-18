43f6ca452fde66642ef5bd7e81e6be9d4c3b0ce7
System Administrator <root@Matthews-MacBook-Pro.local>
2020-07-18 14:44:17 -0500

    Second Commit

--- efs/efs/urls.py
+++ efs/efs/urls.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from django.conf.urls import  include,url
 from django.urls import path
 from django.contrib.auth import views
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
-    url(r'^accounts/login/$', views.LoginView.as_view, name='login'),
-    url(r'^accounts/logout/$', views.LogoutView.as_view, name='logout', kwargs={'next_page': '/'}),
-]
+    url(r'^accounts/login/$', LoginView.as_view(template_name='registration/login.html'), name="login"),
+    url(r'^accounts/logout/$', LogoutView.as_view(), LogoutView.next_page, name="logout"),
+
+   ]
+
 
 ## url(r'^login/$', views.LoginView.as_view(template_name=template_name), name='login'),
